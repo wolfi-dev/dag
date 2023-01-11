877becc9188fd1111a6bd050ca31e8744fdf9f35
Jason Hall <jason@chainguard.dev>
2023-01-11 14:57:03 -0500

    text: emit make commands or makefile lines
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/commands/text.go
+++ pkg/commands/text.go
@@ -6,21 +6,20 @@ import (
 	"log"
 	"os"
 
+	"chainguard.dev/apko/pkg/build/types"
 	"github.com/dominikbraun/graph"
 	"github.com/spf13/cobra"
 	"github.com/wolfi-dev/dag/pkg"
 )
 
 func cmdText() *cobra.Command {
-	var dir, arch string
+	var dir, arch, t string
 	var showDependents bool
 	text := &cobra.Command{
 		Use:   "text",
 		Short: "Print a sorted list of downstream dependent packages",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if arch == "arm64" {
-				arch = "aarch64"
-			}
+			arch := types.ParseArchitecture(arch).ToAPK()
 
 			g, err := pkg.NewGraph(os.DirFS(dir), dir)
 			if err != nil {
@@ -58,33 +57,56 @@ func cmdText() *cobra.Command {
 				g = subgraph
 			}
 
-			err = text(*g, arch, os.Stdout)
-			if err != nil {
-				return err
-			}
-
-			return nil
+			return text(*g, arch, textType(t), os.Stdout)
 		},
 	}
 	text.Flags().StringVarP(&dir, "dir", "d", ".", "directory to search for melange configs")
 	text.Flags().StringVarP(&arch, "arch", "a", "x86_64", "architecture to build for")
 	text.Flags().BoolVarP(&showDependents, "show-dependents", "D", false, "show packages that depend on these packages, instead of these packages' dependencies")
+	text.Flags().StringVarP(&t, "type", "t", string(typeTarget), "What type of text to emit")
 	return text
 }
 
-func text(g pkg.Graph, arch string, w io.Writer) error {
+type textType string
+
+const (
+	typeTarget       = "target"
+	typeMakefileLine = "makefile"
+)
+
+func reverse(ss []string) {
+	last := len(ss) - 1
+	for i := 0; i < len(ss)/2; i++ {
+		ss[i], ss[last-i] = ss[last-i], ss[i]
+	}
+}
+
+func text(g pkg.Graph, arch string, t textType, w io.Writer) error {
 	all, err := g.Sorted()
 	if err != nil {
 		return err
 	}
+	reverse(all)
 
 	for _, node := range all {
-		target, err := g.MakeTarget(node, arch)
-		if err != nil {
-			return err
+		switch t {
+		case typeTarget:
+			target, err := g.MakeTarget(node, arch)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(w, "%s\n", target)
+		case typeMakefileLine:
+			entry, err := g.MakefileEntry(node)
+			if err != nil {
+				return err
+			}
+			if entry != "" {
+				fmt.Fprintf(w, "%s\n", entry)
+			}
+		default:
+			return fmt.Errorf("invalid type: %s", t)
 		}
-
-		fmt.Fprintf(w, "%s\n", target)
 	}
 
 	return nil
